1911c2835efca8a099e3d2dad8539ba49270e05a
刘泽锦 <liuzejin@zhongan.com>
2021-02-26 15:44:02 +0800

    chore: update

--- packages/lzj-react-pack/services/build.js
+++ packages/lzj-react-pack/services/build.js
@@ -1,22 +1,86 @@
-// webpack.prod.js
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { merge } = require('webpack-merge');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const WebpackDevServer = require('webpack-dev-server');
+const fs = require('fs');
+const chalk = require('chalk');
+const clear = require('clear');
 
-const common = require('./webpack.common')();
-const paths = require('../config/paths');
+const config = require('../webpack.config/webpack.prod');
 
-module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    path: paths.appBuild,
-    filename: 'static/js/[name].[contenthash:8].js',
-    chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-  },
-  devtool: 'cheap-module-source-map',
-  optimization: {
-    minimize: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
+process.on('unhandledRejection', err => {
+  throw err;
 });
+
+module.exports = () => {
+  const compiler = webpack(config);
+
+  const now = () => {
+    const time = new Date();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const leftPad = (n) => { return n < 10 ? `0${n}` : n; };
+
+    return `${leftPad(hours)}:${leftPad(minutes)}:${leftPad(seconds)}`;
+  };
+
+  console.info('Glove[build]: START  ', now());
+
+  if (process.env.PROGRESS) {
+    compiler.apply(new ProgressBarPlugin({
+      format: 'Glove[build]: [:bar] :percent (:elapsed seconds) [:msg]',
+      // clear: false,
+      summary: false,
+      customSummary(buildTime) {
+        console.info('Glove[build]: ELAPSED', buildTime);
+      },
+    }));
+  }
+
+  compiler.run((err, state) => {
+    console.info('Glove[build]: FINISH ', now());
+
+    if (err) {
+      return console.error(err);
+    }
+
+    if (state.hasErrors()) {
+      console.info('Glove[build]: ERROR\n');
+      console.info(state.toString({
+        hash: false,
+        version: true,
+        children: true,
+        assets: false,
+        chunks: false,
+        chunkModules: false,
+        colors: true,
+      }));
+      return;
+    }
+
+    console.info('Glove[build]: SUCCES\n');
+    /* eslint-disable no-console */
+    // glove 常规显示方式
+    // console.info(state.toString({
+    //   hash: false,
+    //   version: false,
+    //   assets: false,
+    //   chunkModules: false,
+    //   colors: true,
+    // }));
+
+    // webpack 常规显示方式
+    console.info(state.toString({
+      hash: false,
+      version: true,
+      children: false,
+      assets: true,
+      assetsSort: 'name',
+      chunks: false,
+      chunkModules: false,
+      colors: true,
+      modules: false,
+    }));
+    /* eslint-enable no-console */
+  });
+};
--- packages/lzj-react-pack/webpack.config/webpack.prod.js
+++ packages/lzj-react-pack/webpack.config/webpack.prod.js
@@ -1,5 +1,4 @@
 // webpack.prod.js
-const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 
