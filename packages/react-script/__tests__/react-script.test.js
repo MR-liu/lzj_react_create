d9bfb8248a4a9907b393401226fefcb0b5e64564
刘泽锦 <liuzejin@zhongan.com>
2021-02-26 13:55:06 +0800

    chore: update

--- packages/react-script/__tests__/react-script.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactScript = require('..');
-
-describe('react-script', () => {
-    it('needs tests');
-});
--- packages/react-script/lib/react-script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = reactScript;
-
-function reactScript() {
-    // TODO
-}
