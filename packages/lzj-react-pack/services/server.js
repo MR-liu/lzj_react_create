e4b5d6189d512e38cc86f2966af14249a81b3638
刘泽锦 <best_liuzejin@sina.com>
2021-03-08 23:47:15 +0800

    chore: feat

--- packages/lzj-react-pack/services/server.js
+++ packages/lzj-react-pack/services/server.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = 'development';
 const path = require('path');
 
 process.on('unhandledRejection', err => {
-  throw err;
+    throw err;
 });
 
 const chalk = require('chalk');
@@ -15,51 +15,58 @@ const WebpackDevServer = require('webpack-dev-server');
 const paths = require('../config/paths');
 const config = require('../webpack.config/webpack.dev');
 const choosePort = require('../lib/chooseport');
+const runCLI = require('webpack-cli/lib/bootstrap');
 
-module.exports = async () => {
-  // const compiler = createCompiler({
-  //   appName,
-  //   config,
-  //   devSocket,
-  //   urls,
-  //   useYarn,
-  //   useTypeScript,
-  //   tscCompileOnError,
-  //   webpack,
-  // });
+module.exports = async() => {
+    // const compiler = createCompiler({
+    //   appName,
+    //   config,
+    //   devSocket,
+    //   urls,
+    //   useYarn,
+    //   useTypeScript,
+    //   tscCompileOnError,
+    //   webpack,
+    // });
 
-  // console.log(config);
+    // console.log(config);
 
-  const compiler = webpack(config, (err) => {
-    err && console.log(chalk.cyan(err));
-  });
+    const compiler = webpack(config, (err) => {
+        err && console.log(chalk.cyan(err));
+    });
 
-  const devServerConfig = {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, '../public'),
-    open: false,
-    hot: true,
-    quiet: true,
-  };
+    const devServerConfig = {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, '../public'),
+        open: false,
+        hot: true,
+        quiet: true,
+    };
 
 
-  const devServer = new WebpackDevServer(compiler, devServerConfig);
+    const devServer = new WebpackDevServer(compiler, devServerConfig);
 
-  const port = require(paths.appPackageJson).port || 8001;
-  const _port = await choosePort(port);
-  const HOST = process.env.HOST || '0.0.0.0';
+    const port = require(paths.appPackageJson).port || 8001;
+    const _port = await choosePort(port);
+    const HOST = process.env.HOST || '0.0.0.0';
 
-  devServer.listen(_port, HOST, err => {
-    if (err) {
-      return console.log(err);
-    }
+    runCLI([
+        process.argv[0],
+        process.argv[1],
+        'serve'
+    ], compiler);
 
-    console.warn(
-      chalk.yellow(
-        `Fast Refresh requires React 16.10 or higher. You are using React.`
-      )
-    );
+    // devServer.listen(_port, HOST, err => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
 
-    console.log(chalk.cyan('Starting the development server...\n'));
-  });
+    //     console.warn(
+    //         chalk.yellow(
+    //             `Fast Refresh requires React 16.10 or higher. You are using React.`
+    //         )
+    //     );
+
+    //     console.log(chalk.cyan('Starting the development server...\n'));
+    // });
 }
