49749bd2a6d3a69d89a75ba7e297450b06be32d5
刘泽锦 <liuzejin@zhongan.com>
2021-03-11 18:08:53 +0800

    chore: update

--- packages/lzj-react-pack/webpack.config/webpack.common.js
+++ packages/lzj-react-pack/webpack.config/webpack.common.js
@@ -150,7 +150,7 @@ module.exports = function(){
     },
     plugins: [
       new webpack.ProgressPlugin(),
-      new CopyWebpackPlugin({
+      !!paths.appAsset && new CopyWebpackPlugin({
         patterns: [
           {
             from: paths.appAsset,
