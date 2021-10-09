3e27300a0389870ab39412ec038d7e965252d9b1
刘泽锦 <liuzejin@corp.netease.com>
2021-10-09 11:15:14 +0800

    chore: update

--- packages/lzj-react-pack/webpack.config/devServer.config.js
+++ packages/lzj-react-pack/webpack.config/devServer.config.js
@@ -1,21 +1,39 @@
-const paths = require('../config/paths');
+const paths = require("../config/paths");
 const proxySetting = require(paths.appPackageJson).proxy;
-const choosePort = require('../lib/chooseport');
+const choosePort = require("../lib/chooseport");
 
-module.exports = async() => {
-    const port = require(paths.appPackageJson).port || 8001;
-    const _port = await choosePort(port);
+// module.exports = async() => {
+//     const port = require(paths.appPackageJson).port || 8001;
+//     const _port = await choosePort(port);
 
-    return {
-        compress: true,
-        contentBase: paths.build,
-        proxy: proxySetting,
-        historyApiFallback: true,
-        contentBase: paths.appPublic,
-        hot: true,
-        open: true,
-        quiet: true,
-        port: _port,
-        clientLogLevel: 'silent'
-    }
+//     return {
+//         compress: true,
+//         contentBase: paths.build,
+//         proxy: proxySetting,
+//         historyApiFallback: true,
+//         contentBase: paths.appPublic,
+//         hot: true,
+//         open: true,
+//         quiet: true,
+//         port: _port,
+//         clientLogLevel: 'silent'
+//     }
+// };
+
+module.exports = () => {
+  const port = require(paths.appPackageJson).port || 8001;
+  // const _port = await choosePort(port);
+
+  return {
+    compress: true,
+    contentBase: paths.build,
+    proxy: proxySetting,
+    historyApiFallback: true,
+    contentBase: paths.appPublic,
+    hot: true,
+    open: true,
+    quiet: true,
+    port: port,
+    clientLogLevel: "silent",
+  };
 };
